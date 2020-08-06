a2bf633e1ae06314e4e0fadbeab54fc7ce6e21a5
Harenateweldebrhan <68976395+Harenateweldebrhan@users.noreply.github.com>
2020-08-05 21:32:59 -0400

    Update GitFun.java

--- src/Utility/GitFun.java
+++ src/Utility/GitFun.java
@@ -3,5 +3,6 @@ package Utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Harena Teweldebrhan");
+        // this line is from Github
     }
 }
